8ec775057e7704c66a928ab5d8daf728a95f0a01
nicorradi <121757456+nicorradi@users.noreply.github.com>
2023-02-27 15:58:42 +0100

    Update users.js

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const addressSchema = mongoose.Schema({
     street: String,
     city: String,
-    zipcode : Number,
+    zipcode : String,
     country : String,
    });
 
