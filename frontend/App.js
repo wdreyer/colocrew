afee36f26d80bb8e6adebdf1caa15ee4dd894ebd
Naoëlle <alyiiaah@live.fr>
2023-02-27 16:00:53 +0100

    create AreaChoiceScreen.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import AreaChoiceScreen from './screens/AreaChoiceScreen';
 
 export default function App() {
-  const [showAreaChoiceScreen, setShowAreaChoiceScreen] = React.useState(false);
   
   return (
     <View style={styles.container}>
@@ -17,8 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
   },
 });
--- frontend/screens/AreaChoiceScreen.js
+++ frontend/screens/AreaChoiceScreen.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native'; 
+import { StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native'; 
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 
 export default function AreaChoiceScreen() {
   return (
     
-    <View style={styles.container}>
-      <Text>AreaChoiceScreen</Text>
+    <LinearGradient 
+    style={styles.container}
+    colors= {["#281c47" , "#b8336a"]}>
+        <Image style={styles.image} source={require('../assets/LogoCompletBlanc.png')} />
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity activeOpacity={0.7} style={styles.button} onPress={() => navigation.navigate('TabRecruiterNavigator')}>
+          <Text style={styles.buttonText}>Espace Recruteur</Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7} style={styles.button} onPress={() => navigation.navigate('TabCandidateNavigator')}>
+          <Text style={styles.buttonText}>Espace Candidat</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   );
 }
 
@@ -17,6 +29,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 100,
+    resizeMode: 'contain'
+  },
+ 
+  buttonsContainer: {
+    flexDirection: 'column',
+    marginTop: 100,
+  },
+  button: {
+    backgroundColor: '#FAD4D8',
+    borderColor: '#281C47',
+    borderWidth: '1px',
+    width: 350,
+    height: 55,
     justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 6,
+    marginVertical: 15,
+  },
+  buttonText: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#281C47',
+    fontWeight: 'bold',
   },
 });
