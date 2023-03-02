a2fdef0c031ffb6c291b6020c4a7c19051029b0a
wdreyer <121089571+wdreyer@users.noreply.github.com>
2023-03-02 19:40:19 +0100

    Update AreaChoiceScreen.js

--- frontend/screens/AreaChoiceScreen.js
+++ frontend/screens/AreaChoiceScreen.js
@@ -1,33 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { useSelector, useDispatch } from "react-redux";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native'; 
+import { LinearGradient } from 'expo-linear-gradient';
+import { useSelector, useDispatch } from 'react-redux';
 import { addUserToStore } from "../reducers/users";
-
-import config from "../config";
+import config from '../config';
 
 export default function AreaChoiceScreen({ navigation }) {
-  const user = useSelector((state) => state.users);
-  const dispatch = useDispatch();
+    const user = useSelector((state) => state.users);
+    const dispatch = useDispatch()
 
-  const handleRecruiter = () => {
-    fetch(`${config.URL_BACKEND}/users/updateRole`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        uid: user.token,
-        isRecruiter: true,
-        isCandidate: false,
-      }),
-    })
-      .then((rs) => rs.json())
-      .then((res) => {
-        dispatch(
-          addUserToStore({
-            isConnected: true,
-            email: res.userUpdated.email,
+    const handleRecruiter = () => {
+      fetch(`${config.URL_BACKEND}/users/updateRole`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          uid : user.uid,
+          isRecruiter : true,
+          isCandidate : false,
+      
+        })
+      }).then(rs => rs.json())
+      .then(res => {           
+         dispatch(addUserToStore({
+            isConnected : true,
+            email : res.userUpdated.email,
             uid: res.userUpdated.uid,
             isCandidate: res.userUpdated.isCandidate,
             isRecruiter: res.userUpdated.isRecruiter,
@@ -43,7 +41,7 @@ export default function AreaChoiceScreen({ navigation }) {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          uid : user.token,
+          uid : user.uid,
           isRecruiter : false,
           isCandidate : true,      
         })
@@ -60,59 +58,20 @@ export default function AreaChoiceScreen({ navigation }) {
     navigation.navigate('TabCandidateNavigator')
     }
 
-          })
-        );
-      });
-    navigation.navigate("TabRecruiterNavigator");
-  };
 
-  const handleCandidate = () => {
-    fetch(`${config.URL_BACKEND}/users/updateRole`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        uid: user.token,
-        isRecruiter: false,
-        isCandidate: true,
-      }),
-    })
-      .then((rs) => rs.json())
-      .then((res) => {
-        dispatch(
-          addUserToStore({
-            isConnected: true,
-            email: res.userUpdated.email,
-            uid: res.userUpdated.uid,
-            isCandidate: res.userUpdated.isCandidate,
-            isRecruiter: res.userUpdated.isRecruiter,
-          })
-        );
-      });
-    navigation.navigate("TabCandidateNavigator");
-  };
+
 
   return (
-    <LinearGradient style={styles.container} colors={["#281c47", "#b8336a"]}>
-      <Image
-        style={styles.image}
-        source={require("../assets/LogoCompletBlanc.png")}
-      />
+    <LinearGradient 
+    style={styles.container}
+    colors= {["#281c47" , "#b8336a"]}>
+        <Image style={styles.image} source={require('../assets/LogoCompletBlanc.png')} />
       <StatusBar style="light" />
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity
-          activeOpacity={0.7}
-          style={styles.button}
-          onPress={() => handleRecruiter()}
-        >
+        <TouchableOpacity activeOpacity={0.7} style={styles.button} onPress={() => handleRecruiter()}>
           <Text style={styles.buttonText}>Espace Recruteur</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.7}
-          style={styles.button}
-          onPress={() => handleCandidate()}
-        >
+        <TouchableOpacity activeOpacity={0.7} style={styles.button} onPress={() => handleCandidate() }>
           <Text style={styles.buttonText}>Espace Candidat</Text>
         </TouchableOpacity>
       </View>
@@ -123,36 +82,36 @@ export default function AreaChoiceScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   image: {
     width: 200,
     height: 200,
     marginTop: 100,
-    resizeMode: "contain",
+    resizeMode: 'contain'
   },
-
+ 
   buttonsContainer: {
-    flexDirection: "column",
+    flexDirection: 'column',
     marginTop: 100,
   },
   button: {
-    backgroundColor: "#FAD4D8",
-    borderColor: "#281C47",
+    backgroundColor: '#FAD4D8',
+    borderColor: '#281C47',
     borderWidth: 1,
     width: 350,
     height: 55,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: 6,
     marginVertical: 15,
   },
   buttonText: {
     fontSize: 20,
-    textAlign: "center",
-    color: "black",
-    fontWeight: "bold",
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
   },
 });
